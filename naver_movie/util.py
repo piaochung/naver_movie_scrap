09f0ec47ea79336c55b6f956737c9319710b531c
chungchung <hangjastar@gmail.com>
2021-05-01 18:41:59 +0900

    add clean_text

--- naver_movie/util.py
+++ naver_movie/util.py
@@ -10,3 +10,8 @@ def get_soup(target_url):
     html = urllib.request.urlopen(target_url).read()
     soup = BeautifulSoup(html, 'html.parser')
     return soup
+
+
+def clean_text(text):
+    code = re.findall("\d+", text)
+    return code[0]
